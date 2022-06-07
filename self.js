f030606fea5f8cef23234f10f76653a70917ed8a
moderasyonbo <93944142+moderasyonbo@users.noreply.github.com>
2022-06-07 19:23:07 +0300

    Add files via upload

---
+++ self.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { Client, RichEmbed } = require("discord.js");
+const app = express();
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
+}
+const http = require("http");
+app.get("/", (request, response) => {
+  console.log(Date.now() + "");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const data = new Map();
+
+
+client.login("tokeninizi girin.");
